d99509b1bf8ebfa587de2e6da712bdfd746baabc
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2011-11-03 00:57:34 +0000

    Moved the DynamicXContainer class out of the unit test file into the mai project now it has a basic level of functionality.

--- Dynamic.Xml.Tests.Unit/XmlLoadingTests.cs
+++ Dynamic.Xml.Tests.Unit/XmlLoadingTests.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.IO;
 using System.Linq;
-using System.Xml;
-using System.Xml.Linq;
 using NUnit.Framework;
 
 namespace Dynamic.Xml.Tests.Unit
@@ -110,72 +106,5 @@ namespace Dynamic.Xml.Tests.Unit
         {
             return @"<Document Type=""Simple""><Response Data=""Hello"" /><Response Data=""Goodbye"" /></Document>";
         }
-    }
-
-    public class DynamicXContainer : DynamicObject
-    {
-        public static dynamic Parse(string xml)
-        {
-            return new DynamicXContainer(XDocument.Parse(xml));
-        }
-
-        private readonly XContainer _container;
-
-        private DynamicXContainer(XContainer container)
-        {
-            _container = container;
-        }
-
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
-        {
-            if (binder.Name.StartsWith("Has"))
-            {
-                string elementName = binder.Name.Substring(3);
-                result = GetAttributes(elementName).Any() || GetElements(elementName).Any();
-            }
-            else if (binder.Name.StartsWith("Every"))
-            {
-                string elementName = binder.Name.Substring(5);
-                result = GetElements(elementName);
-            }
-            else
-            {
-                var attribute = GetAttributes(binder.Name).FirstOrDefault();
-
-                if (attribute != null)
-                {
-                    result = attribute.Value;
-                }
-                else
-                {
-                    result = GetElements(binder.Name).FirstOrDefault();
-                    if (result == null)
-                    {
-                        throw new XmlException(string.Format("There is no \"{0}\" attribute or element in the following XML:\r\n\r\n{1}", binder.Name, _container));
-                    }
-                }
-            }
-
-            return result != null;
-        }
-
-        private IEnumerable<XAttribute> GetAttributes(string attributeName)
-        {
-            var element = _container as XElement;
-
-            if (element == null) return Enumerable.Empty<XAttribute>();
-
-            return element.Attributes()
-                .Where(a => a.Name.ToString().Equals(attributeName, StringComparison.InvariantCultureIgnoreCase))
-                .Select(a => a);
-        }
-
-        private IEnumerable<DynamicXContainer> GetElements(string elementName)
-        {
-            return _container.Elements()
-                .Where(e => e.Name.ToString().Equals(elementName, StringComparison.InvariantCultureIgnoreCase))
-                .Select(e => new DynamicXContainer(e));
-        }
-
     }
 }
---
+++ Dynamic.Xml/DynamicXContainer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Dynamic.Xml
+{
+    public class DynamicXContainer : DynamicObject
+    {
+        private readonly XContainer _container;
+
+        public static dynamic Parse(string xml)
+        {
+            return new DynamicXContainer(XDocument.Parse(xml));
+        }
+
+        private DynamicXContainer(XContainer container)
+        {
+            _container = container;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (binder.Name.StartsWith("Has"))
+            {
+                string elementName = binder.Name.Substring(3);
+                result = GetAttributes(elementName).Any() || GetElements(elementName).Any();
+            }
+            else if (binder.Name.StartsWith("Every"))
+            {
+                string elementName = binder.Name.Substring(5);
+                result = GetElements(elementName);
+            }
+            else
+            {
+                var attribute = GetAttributes(binder.Name).FirstOrDefault();
+
+                if (attribute != null)
+                {
+                    result = attribute.Value;
+                }
+                else
+                {
+                    result = GetElements(binder.Name).FirstOrDefault();
+                    if (result == null)
+                    {
+                        throw new XmlException(string.Format("There is no \"{0}\" attribute or element in the following XML:\r\n\r\n{1}", binder.Name, _container));
+                    }
+                }
+            }
+
+            return result != null;
+        }
+
+        private IEnumerable<XAttribute> GetAttributes(string attributeName)
+        {
+            var element = _container as XElement;
+
+            if (element == null) return Enumerable.Empty<XAttribute>();
+
+            return element.Attributes()
+                .Where(a => a.Name.ToString().Equals(attributeName, StringComparison.InvariantCultureIgnoreCase))
+                .Select(a => a);
+        }
+
+        private IEnumerable<DynamicXContainer> GetElements(string elementName)
+        {
+            return _container.Elements()
+                .Where(e => e.Name.ToString().Equals(elementName, StringComparison.InvariantCultureIgnoreCase))
+                .Select(e => new DynamicXContainer(e));
+        }
+
+    }
+}
